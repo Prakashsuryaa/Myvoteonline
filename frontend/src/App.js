30b39dc5d38cf287be85006f981d45b493f7f116
Bhushan9090 <bhushan905306@gmail.com>
2025-02-10 16:14:40 +0530

    addlazy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./component/login.jsx";
-import Register from "./component/register.jsx";
-import Navbar from "./component/navbar.jsx";
-import Footer from "./component/footer.jsx";
-import Dashboard from "./pages/dashboard.jsx";
-import VoterForm from "./component/voterform.jsx";
-import SearchPage from "./component/seachpage.jsx";
+import { Suspense, lazy } from 'react';
+//lazy load the component 
+
+const Register = lazy(()=> import ('./component/register.jsx'))
+const Footer = lazy(()=> import ('./component/footer.jsx'))
+const Dashboard = lazy(()=> import ('./pages/dashboard.jsx'))
+const VoterForm = lazy(()=> import ('./component/voterform.jsx'))
+const SearchPage = lazy(()=> import ('./component/seachpage.jsx'))
+const Login = lazy(()=> import ('./component/login.jsx'))
+const Navbar = lazy(() => import('./component/navbar.jsx'))
+
 function App() {
   return (
     <Router>
+
     
       <Routes>
         <Route path="/" element={<Login />} />
