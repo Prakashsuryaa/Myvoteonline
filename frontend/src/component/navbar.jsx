1b7bc37092459406ecfb9a45eaea9a7dfa412585
Sandip9528 <sandipravan95@gmail.com>
2025-02-13 13:02:14 +0530

    add navigation

--- frontend/src/component/navbar.jsx
+++ frontend/src/component/navbar.jsx
@@ -3,8 +3,10 @@ import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
 import img from "../../src/images/Img1.png";
 import img3 from "../../src/images/img3.png";
 import img4 from "../../src/images/img4.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -71,20 +73,22 @@ const Navbar = () => {
         <button className="md:hidden text-[#165887]" onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
-        <ul
-          className={`md:flex md:space-x-10 lg:space-x-20 text-lg font-semibold cursor-pointer absolute md:static bg-[#E8FAFC] md:bg-transparent w-full md:w-auto left-0 top-12 md:top-0 transition-all duration-300 ease-in-out ${
-            menuOpen ? "block" : "hidden"
-          }`}
-        >
-          {["Home", "About Us", "Services", "Form", "Election"].map((item) => (
-            <li key={item} className="px-4 py-2 md:px-0 md:py-0 hover:bg-gray-200 md:hover:bg-transparent">
-              {item}
+        <ul className="flex space-x-20 text-lg font-semibold cursor-pointer">
+          {[
+            { name: "Home", path: "/dashboard" },
+            { name: "About Us", path: "/aboutus" },
+            { name: "Services", path: "/services" },
+            { name: "Form", path: "/form" },
+            { name: "Search Pages", path: "/search-page" },
+          ].map((item) => (
+            <li key={item.name} onClick={() => navigate(item.path)} className="hover:text-blue-700 transition">
+              {item.name}
             </li>
           ))}
         </ul>
         <button className="hidden md:flex items-center gap-2 text-[#165887] text-sm px-4 md:px-6 py-2 rounded-2xl bg-[#FBF6F6]">
           <FaSearch aria-label="Search" />
-          <span>Search Pages</span>
+          <input type="search" placeholder="Search Page" className="border-none outline-none" />
         </button>
       </nav>
     </>
